3c53e49fa48ffcbd64ae8e1298a1b471a2618900
DudaGod <dudkevich@yandex-team.ru>
2021-03-31 16:54:11 +0300

    fix(w3c): "getAttribute", "getElementSize" and "moveTo" commands

--- test/lib/commands/property/getAttribute.js
+++ test/lib/commands/property/getAttribute.js
@@ -31,7 +31,6 @@ describe('"getAttribute" command', () => {
     });
 
     it('should call "getAttribute" on browser element with passed attribute name', async () => {
-        const browser = mkBrowser_();
         const element = mkElement_();
 
         findElement.withArgs(browser, '.some-selector').resolves(element);
@@ -40,5 +39,75 @@ describe('"getAttribute" command', () => {
         await browser.getAttribute('.some-selector', 'method');
 
         assert.calledOnceWithExactly(element.getAttribute, 'method');
+    });
+
+    describe('browser does not support w3c protocol', () => {
+        beforeEach(() => {
+            browser.isW3C = false;
+        });
+
+        ['href', 'action'].forEach((attrName) => {
+            [
+                {name: 'relative url without protocol', url: '//ya.ru'},
+                {name: 'relative url without protocol and host', url: '/relative'}
+            ].forEach(({name, url}) => {
+                it(`should not transform ${name} of "${attrName}" attribute`, async () => {
+                    const element = mkElement_();
+                    element.getAttribute.withArgs(attrName).resolves(url);
+
+                    findElement.withArgs(browser, '.some-selector').resolves(element);
+                    addGetAttribute(browser);
+
+                    const result = await browser.getAttribute('.some-selector', attrName);
+
+                    assert.equal(result, url);
+                });
+            });
+        });
+    });
+
+    describe('browser support w3c protocol', () => {
+        beforeEach(() => {
+            browser.isW3C = true;
+        });
+
+        it('should not transform url of not "href" or "action" attributes', async () => {
+            const element = mkElement_();
+            element.getAttribute.withArgs('some-attr').resolves('/relative');
+
+            findElement.withArgs(browser, '.some-selector').resolves(element);
+            addGetAttribute(browser);
+
+            const result = await browser.getAttribute('.some-selector', 'some-attr');
+
+            assert.equal(result, '/relative');
+        });
+
+        ['href', 'action'].forEach((attrName) => {
+            [
+                {
+                    name: 'relative url without protocol',
+                    url: '//ya.ru',
+                    expectedUrl: 'https://ya.ru'},
+                {
+                    name: 'relative url without protocol and host',
+                    url: '/relative',
+                    expectedUrl: 'https://foo.bar/relative'
+                }
+            ].forEach(({name, url, expectedUrl}) => {
+                it(`should transform ${name} of "${attrName}" attribute`, async () => {
+                    const element = mkElement_();
+                    element.getAttribute.withArgs(attrName).resolves(url);
+
+                    findElement.withArgs(browser, '.some-selector').resolves(element);
+                    browser.getUrl.resolves('https://foo.bar');
+                    addGetAttribute(browser);
+
+                    const result = await browser.getAttribute('.some-selector', attrName);
+
+                    assert.equal(result, expectedUrl);
+                });
+            });
+        });
     });
 });
