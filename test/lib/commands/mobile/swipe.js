6991eb9863e6520b5dbd626cd5957f0ec240e346
Nikolai Markov <shadowusr@yandex-team.ru>
2023-06-27 17:52:29 +0300

    chore: implement tests for updated buttonUp, buttonDown, swipe and touch commands

--- test/lib/commands/mobile/swipe.js
+++ test/lib/commands/mobile/swipe.js
@@ -4,13 +4,15 @@ const proxyquire = require('proxyquire');
 const {mkBrowser_, mkElement_} = require('../../../utils');
 
 describe('"swipe" command', () => {
-    let browser, findElement, addSwipe;
+    let browser, findElement, addSwipe, getPerformActionId;
 
     beforeEach(() => {
         browser = mkBrowser_();
         findElement = sinon.stub().resolves(mkElement_());
+        getPerformActionId = sinon.stub().returns('some-id');
         addSwipe = proxyquire('lib/commands/mobile/swipe', {
-            '../../helpers/findElement': findElement
+            '../../helpers/findElement': findElement,
+            '../../helpers/getPerformActionId': getPerformActionId
         });
     });
 
@@ -55,8 +57,9 @@ describe('"swipe" command', () => {
         assert.calledOnceWithExactly(findElement, browser, '.some-selector');
     });
 
-    it('should call "touchFlick" with offsets, found element id and speed', async () => {
+    it('should call "touchFlick" with offsets, found element id and speed in non W3C mode', async () => {
         const browser = mkBrowser_();
+        browser.isW3C = false;
         const element = mkElement_({id: 123});
 
         findElement.withArgs(browser, '.some-selector').resolves(element);
@@ -65,5 +68,29 @@ describe('"swipe" command', () => {
         await browser.swipe('.some-selector', 100, 200, 300);
 
         assert.calledOnceWithExactly(browser.touchFlick, 100, 200, 123, 300);
+    });
+
+    it('should call performActions in W3C mode', async () => {
+        const browser = mkBrowser_();
+        browser.isW3C = true;
+        const element = mkElement_({id: 123});
+
+        getPerformActionId.withArgs(browser).returns('some-pointer-id');
+        findElement.withArgs(browser, '.some-selector').resolves(element);
+        addSwipe(browser);
+
+        await browser.swipe('.some-selector', 100, 0, 100);
+
+        assert.calledOnceWithExactly(browser.performActions, [{
+            type: 'pointer',
+            id: getPerformActionId(browser),
+            parameters: {pointerType: 'touch'},
+            actions: [
+                {type: 'pointerMove', duration: 0, origin: element, x: 0, y: 0},
+                {type: 'pointerDown', button: 0},
+                {type: 'pointerMove', duration: 1000, origin: element, x: 100, y: 0},
+                {type: 'pointerUp', button: 0}
+            ]
+        }]);
     });
 });
